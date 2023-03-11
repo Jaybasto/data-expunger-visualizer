9787140a1e0220dfe914092919df36556a369abc
Christopher-Pettinari <103431761+Christopher-Pettinari@users.noreply.github.com>
2023-03-11 14:09:53 -0500

    File upload for front end

---
+++ App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import React, { Component } from 'react';
+import FilesUploadComponent from './files-upload-component';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <div className="App-header">
+        <FilesUploadComponent />
+      </div>
+
+      </header>
+    </div>
+
+  );
+}
+
+export default App;
